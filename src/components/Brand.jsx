542fd1bea04613bb7a2f51a08f56ec605f662927
Noor1805 <kashafnoor346@gmail.com>
2025-07-20 15:43:21 +0530

    Updated Brand component and added BrandAnimation

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { brandImageAnimation } from "@/animations/BrandAnimation";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -36,7 +37,7 @@ export default function Brand() {
     const rotateTl = gsap.timeline({
       scrollTrigger: {
         trigger: cube,
-        start: "top 10%",
+        start: "top 15%",
         end: "bottom 125%",
         scrub: 2,
       },
@@ -48,101 +49,31 @@ export default function Brand() {
       ease: "power2.inOut",
       duration: 2,
     });
+    const cleanupImages = brandImageAnimation();
 
-    return () => ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+      cleanupImages(); 
+    };
   }, []);
 
   return (
     <div className="bg-gray-200 px-2 z-0 overflow-x-hidden hide-scrollbar">
-      {/* Mobile/Small layout without animation */}
       <div className="block md:hidden py-10">
-  {/* For better width handling from 375px to 639px */}
-  <div className="mx-auto w-full max-w-[90%] space-y-8">
-
-    {/* Front Face */}
-    <div className="bg-amber-500 text-white rounded-[30px] px-4 sm:px-6 py-8 flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-4">
-      
-      {/* Text Section */}
-      <div className="w-full sm:w-[65%] sm:pl-4">
-        <p className="sm:text-lg font-semibold text-gray-800">Behind The Brand</p>
-        <h2 className="sm:text-xl font-semibold leading-snug">
-          We are not afraid.
-          <br /> we are your partners.
-          <br /> we build unique with you.
-        </h2>
-      </div>
-
-      {/* Image Section */}
-      <div className="relative bg-white w-36 h-36 sm:w-40 sm:h-40 rounded-md flex-shrink-0">
-        <img
-          src="/assets/Rectangle 4206.png"
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-        />
-        <img src="/assets/Rectangle 4207.png" className="absolute bottom-8 right-10" />
-        <img src="/assets/Ellipse 60.png" className="absolute bottom-10 right-6" />
-        <img src="/assets/Polygon 1.png" className="absolute top-8 left-6" />
-      </div>
-    </div>
-
-    {/* Bottom Face */}
-    <div className="bg-[#fff6db] text-black rounded-[30px] px-4 sm:px-6 py-8 flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-4">
-
-      {/* Text Section */}
-      <div className="w-full sm:w-[65%] sm:pl-4">
-        <p className="sm:text-lg font-semibold text-gray-800">Behind The Brand</p>
-        <p className="sm:text-xl font-semibold">
-          we <span className="text-yellow-500">blend design</span>, tech,
-          <br /> and creativity to build <br />
-          <span className="text-yellow-500">products</span> that put <br /> customers first.
-        </p>
-      </div>
-
-      {/* Image Section */}
-      <div className="border border-yellow-500 relative w-[190px] h-[190px] rounded-md overflow-hidden flex-shrink-0">
-        <div className="relative w-full h-full p-4">
-          <img
-            src="/assets/Rectangle 4206.png"
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-[60deg]"
-          />
-          <img src="/assets/Rectangle 4207.png" className="absolute bottom-8 right-10" />
-          <img src="/assets/Ellipse 60.png" className="absolute bottom-6 right-14" />
-          <img src="/assets/Polygon 1.png" className="absolute top-6 left-14" />
-        </div>
-      </div>
-    </div>
-
-  </div>
-</div>
-
-
-
-      {/* 3D Cube layout for md and above */}
-      <div
-        style={{ perspective: "1400px" }}
-        className="hidden md:flex justify-center items-center h-[120vh]"
-      >
-        <div
-          ref={cubeRef}
-          style={{ transformStyle: "preserve-3d" }}
-          className="relative w-[80%] xl:w-[82%] h-[30rem] mb-[6rem] rounded-[40px] transform-style-preserve-3d"
-        >
-          {/* Front Face */}
-          <div
-            style={{ transform: "translateZ(244px)" }}
-            className="absolute w-full h-full rounded-[40px] bg-amber-500 px-20 flex flex-row items-center justify-between text-white"
-          >
-            <div ref={textRef} className="max-w-[60%]">
-              <p className="text-[28px] lg:text-[45px] xl:text-[50px] font-semibold text-gray-800">
+        <div className="mx-auto w-full max-w-[90%] space-y-8">
+          <div className="bg-amber-500 text-white rounded-[30px] px-4 sm:px-6 py-8 flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-4">
+            <div className="w-full sm:w-[65%] sm:pl-4">
+              <p className="sm:text-lg font-semibold text-gray-800">
                 Behind The Brand
               </p>
-              <h2 className="text-[28px] lg:text-[33px] xl:text-[35px] font-semibold leading-snug">
+              <h2 className="sm:text-xl font-semibold leading-snug">
                 We are not afraid.
                 <br /> we are your partners.
                 <br /> we build unique with you.
               </h2>
             </div>
 
-            <div className="bg-white rounded-md px-10 relative w-49 h-48 ml-7 mr-[35px]">
+            <div className="relative bg-white w-36 h-36 sm:w-40 sm:h-40 rounded-md flex-shrink-0">
               <img
                 src="/assets/Rectangle 4206.png"
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
@@ -162,7 +93,87 @@ export default function Brand() {
             </div>
           </div>
 
-          {/* Bottom Face */}
+          <div className="bg-[#fff6db] text-black rounded-[30px] px-4 sm:px-6 py-8 flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-4">
+            <div className="w-full sm:w-[65%] sm:pl-4">
+              <p className="sm:text-lg font-semibold text-gray-800">
+                Behind The Brand
+              </p>
+              <p className="sm:text-xl font-semibold">
+                we <span className="text-yellow-500">blend design</span>, tech,
+                <br /> and creativity to build <br />
+                <span className="text-yellow-500">products</span> that put{" "}
+                <br /> customers first.
+              </p>
+            </div>
+
+            <div className="border border-yellow-500 relative w-[190px] h-[190px] rounded-md overflow-hidden flex-shrink-0">
+              <div className="relative w-full h-full p-4">
+                <img
+                  src="/assets/Rectangle 4206.png"
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-[60deg]"
+                />
+                <img
+                  src="/assets/Rectangle 4207.png"
+                  className="absolute bottom-8 right-10"
+                />
+                <img
+                  src="/assets/Ellipse 60.png"
+                  className="absolute bottom-6 right-14"
+                />
+                <img
+                  src="/assets/Polygon 1.png"
+                  className="absolute top-6 left-14"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{ perspective: "1400px" }}
+        className="hidden md:flex justify-center items-center h-[120vh]"
+      >
+        <div
+          ref={cubeRef}
+          style={{ transformStyle: "preserve-3d" }}
+          className="relative w-[80%] xl:w-[82%] h-[30rem] mb-[6rem] rounded-[40px] transform-style-preserve-3d"
+        >
+          <div
+            style={{ transform: "translateZ(244px)" }}
+            className="absolute w-full h-full rounded-[40px] bg-amber-500 px-20 flex flex-row items-center justify-between text-white"
+          >
+            <div ref={textRef} className="max-w-[60%]">
+              <p className="text-[28px] lg:text-[45px] xl:text-[50px] font-semibold text-gray-800">
+                Behind The Brand
+              </p>
+              <h2 className="text-[28px] lg:text-[33px] xl:text-[35px] font-semibold leading-snug">
+                We are not afraid.
+                <br /> we are your partners.
+                <br /> we build unique with you.
+              </h2>
+            </div>
+
+            <div className=" bg-white rounded-md px-10 relative w-49 h-48 ml-7 mr-[35px]">
+              <img
+                src="/assets/Rectangle 4206.png"
+                className="brand-img absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              />
+              <img
+                src="/assets/Rectangle 4207.png"
+                className="brand-img absolute bottom-8 right-10"
+              />
+              <img
+                src="/assets/Ellipse 60.png"
+                className="brand-img absolute bottom-10 right-6"
+              />
+              <img
+                src="/assets/Polygon 1.png"
+                className="brand-img absolute top-8 left-6"
+              />
+            </div>
+          </div>
+
           <div
             style={{
               transform: "rotateX(90deg) translateZ(244px)",
@@ -186,19 +197,19 @@ export default function Brand() {
               <div className="relative w-full h-full p-[2rem]">
                 <img
                   src="/assets/Rectangle 4206.png"
-                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-[60deg]"
+                  className="brand-img absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-[60deg]"
                 />
                 <img
                   src="/assets/Rectangle 4207.png"
-                  className="absolute bottom-8 right-10"
+                  className=" brand-img absolute bottom-8 right-10"
                 />
                 <img
                   src="/assets/Ellipse 60.png"
-                  className="absolute bottom-6 right-14"
+                  className="brand-img absolute bottom-6 right-14"
                 />
                 <img
                   src="/assets/Polygon 1.png"
-                  className="absolute top-6 left-14"
+                  className="brand-img absolute top-6 left-14"
                 />
               </div>
             </div>
